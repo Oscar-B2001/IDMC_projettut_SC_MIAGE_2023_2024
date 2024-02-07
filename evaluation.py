9da2b8b5ae262d647294dcad34647c4374fbf197
Oscar-B2001 <oscar.bidou.2001@gmail.com>
2024-02-07 18:29:47 +0100

    DTW + MSE

--- evaluation.py
+++ evaluation.py
@@ -5,7 +5,10 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import MDS
 from sklearn.manifold import TSNE
 
-#df_merged = df_sync.append(def_rel, ignore_index=True)
+from scipy.spatial.distance import euclidean
+from scipy.signal import find_peaks
+
+#df_merged = df_syn.append(def_rel, ignore_index=True)
 #df_merged.sort_values(by=['temps])
 #df_merged --> objet+nom+verbe
 
@@ -65,3 +68,50 @@ def t_sne(df_merged, perplexity):
     tsne = TSNE(n_components=2, perplexity=perplexity).fit(df_merged)
     return tsne
 
+################# MSE / DTW ##########################
+
+def MSE(s_1, s_2):
+    """
+    Cette fonction évalue la MSE entre deux série
+
+    Paramètres:
+    s_1, s_2  --> list/numpy/tuple, deux série temporel
+
+    Return :
+    int, distance euclédienne carrée entre deux série temporel 
+    """
+    if len(s_1) != len(s_2) :
+        raise ValueError('len(df_rel) != len(df_syn)')
+    else :
+        return np.mean(np.square(np.subtract(s_1, s_2)))
+
+
+def DTW(s_1, s_2):
+    """
+    Calcule la distance DTW entre deux séries temporelles s_1 et s_2.
+
+    Paramètres:
+    s_1, s_2  --> list/numpy/tuple, deux série temporel
+
+    Return :
+    dtw_matrix[len_s1, len_s2] --> int, distance DTW entre s_1 et s_2
+    """
+    # Calculer la taille des séries temporelles
+    len_s1, len_s2 = len(s_1), len(s_2)
+
+    # Initialiser la matrice de coût DTW avec des valeurs infinies
+    dtw_matrix = np.zeros((len_s1 + 1, len_s2 + 1))
+    dtw_matrix[:, 0] = np.inf
+    dtw_matrix[0, :] = np.inf
+    dtw_matrix[0, 0] = 0
+
+    # Remplir la matrice de coût DTW
+    for i in range(1, len_s1 + 1):
+        for j in range(1, len_s2 + 1):
+            cost = abs(s_1[i - 1] - s_2[j - 1])  # Calcul du coût local
+            dtw_matrix[i, j] = cost + min(dtw_matrix[i - 1, j],    # Coût de déplacement vers le haut
+                                           dtw_matrix[i, j - 1],    # Coût de déplacement vers la gauche
+                                           dtw_matrix[i - 1, j - 1])  # Coût de déplacement en diagonale
+
+    # Retourner la distance DTW entre les deux séries temporelles
+    return dtw_matrix[len_s1, len_s2]
